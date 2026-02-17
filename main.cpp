074071d4a2459ba50c438c451bd476cc4cb0f5aa
Sroemkhu <Sroemkhunnn@gmail.com>
2026-02-18 00:00:46 +0700

    Game slot

--- main.cpp
+++ main.cpp
@@ -1,42 +1,66 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-
-int main() {
-    srand(time(0));
-    int money = 100;
-    int bet;
-
-    cout << "===== SLOT GAME =====\n";
-
-    while (money > 0) {
-        cout << "\nMoney: " << money << endl;
-        cout << "Enter bet (0 to quit): ";
-        cin >> bet;
-
-        if (bet == 0) break;
-
-        if (bet > money) {
-            cout << "Not enough money!\n";
-            continue;
-        }
-
-        int s1 = rand() % 3;
-        int s2 = rand() % 3;
-        int s3 = rand() % 3;
-
-        cout << s1 << " | " << s2 << " | " << s3 << endl;
-
-        if (s1 == s2 && s2 == s3) {
-            cout << "JACKPOT!\n";
-            money += bet * 2;
-        } else {
-            cout << "Lose!\n";
-            money -= bet;
-        }
-    }
-
-    cout << "Game Over\n";
-    return 0;
+#include <iostream.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	
+	int x,a, b, c, token=4;
+	srand(time(0));
+	
+	cout<<"\t********************************************************\n"
+	    <<"\t*              Welcome to slot machine.                *\n"
+		<<"\t*  Would you like to play? (1 to play, 2 not to play)  *\n"
+		<<"\t********************************************************\n\n";
+	cin>>x;
+	while(token!=0)
+	{cout<<"You have "<<token<< " tokens\n\n"
+	    <<"Pull? (1 to pull, 2 not to pull)\n\n";
+	cin>>x;
+	
+	
+	
+	
+	if(x==1)
+{	
+	
+	a = 1+rand() %10;
+	b = 1+rand() %10;
+	c = 1+rand() %10;
+	
+	
+	cout<<"\t\t"<<a<<"          "<<b<<"          "<<c<<"\n\n";
+	
+}
+		else
+		cout<<"OK\n";
+		{		
+				
+		if(a==b==c)
+		{
+			
+			token+=4;
+			cout<<"You win\n\n";
+		}		
+		if(a==b || b==c || a==c)
+		{
+			token+=1;
+		
+		cout<<"You got two out of three\n\n";
+		
+		
+		}
+		else
+		{
+			token-=1;
+			
+		
+		cout<<"You loose\n\n";
+		}
+		
+		}
+	}
+		
+		
+				
+			return 0;
 }
